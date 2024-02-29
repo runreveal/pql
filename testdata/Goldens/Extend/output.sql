ab63f39a3043038c872e6fd69d8c406c36262e32
Evan Johnson <evan@runreveal.com>
2024-02-28 22:48:19 -0600

    Fix extend tests

--- testdata/Goldens/Extend/output.sql
+++ testdata/Goldens/Extend/output.sql
@@ -1,1 +1,2 @@
-SELECT *, 1 AS "foo" FROM "StormEvents";
+WITH "__subquery0" AS (SELECT "State" AS "State", "EventType" AS "EventType", "DamageProperty" AS "DamageProperty" FROM "StormEvents")
+SELECT *, 1 AS "foo" FROM "__subquery0";
