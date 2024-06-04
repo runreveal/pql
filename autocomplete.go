1117ccd58df9e66bd2bd832b15ad590fd04ef77f
Ross Light <ross@256lights.llc>
2024-06-04 10:08:09 -0700

    Add basic autocomplete API
    Completes table names on the first part of the expression.

---
+++ autocomplete.go
@@ -0,0 +1,95 @@
+// Copyright 2024 RunReveal Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+package pql
+
+import (
+	"cmp"
+	"slices"
+	"strings"
+
+	"github.com/runreveal/pql/parser"
+)
+
+type AnalysisContext struct {
+	Tables map[string]*AnalysisTable
+}
+
+type AnalysisTable struct {
+	Columns []*AnalysisColumn
+}
+
+type AnalysisColumn struct {
+	Name string
+}
+
+type Completion struct {
+	Identifier string
+}
+
+func (ctx *AnalysisContext) SuggestCompletions(source string, cursor parser.Span) []*Completion {
+	pos := cursor.End
+	posSpan := parser.Span{Start: pos, End: pos}
+
+	tokens := parser.Scan(source)
+	expr, _ := parser.Parse(source)
+	if expr == nil {
+		prefix := completionPrefix(source, tokens, pos)
+		result := make([]*Completion, 0, len(ctx.Tables))
+		for tableName := range ctx.Tables {
+			if strings.HasPrefix(tableName, prefix) {
+				result = append(result, &Completion{
+					Identifier: tableName,
+				})
+			}
+		}
+		return result
+	}
+
+	if posSpan.Overlaps(expr.Source.Span()) {
+		// Assume that this is a table name.
+		prefix := completionPrefix(source, tokens, pos)
+		result := make([]*Completion, 0, len(ctx.Tables))
+		for tableName := range ctx.Tables {
+			if strings.HasPrefix(tableName, prefix) {
+				result = append(result, &Completion{
+					Identifier: tableName,
+				})
+			}
+		}
+		return result
+	}
+
+	// TODO(now): More.
+
+	return nil
+}
+
+func completionPrefix(source string, tokens []parser.Token, pos int) string {
+	if len(tokens) == 0 {
+		return ""
+	}
+	i, _ := slices.BinarySearchFunc(tokens, pos, func(tok parser.Token, pos int) int {
+		return cmp.Compare(tok.Span.Start, pos)
+	})
+	i = min(i, len(tokens)-1)
+	if tokens[i].Span.End < pos || !isCompletableToken(tokens[i].Kind) {
+		// Cursor is not adjacent to token. Assume there's whitespace.
+		return ""
+	}
+	start := tokens[i].Span.Start
+	if tokens[i].Kind == parser.TokenQuotedIdentifier {
+		// Skip past initial backtick.
+		start += len("`")
+	}
+	return source[start:pos]
+}
+
+func isCompletableToken(kind parser.TokenKind) bool {
+	return kind == parser.TokenIdentifier ||
+		kind == parser.TokenQuotedIdentifier ||
+		kind == parser.TokenAnd ||
+		kind == parser.TokenOr ||
+		kind == parser.TokenIn ||
+		kind == parser.TokenBy
+}
---
+++ autocomplete_test.go
@@ -0,0 +1,111 @@
+// Copyright 2024 RunReveal Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+package pql
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+	"github.com/runreveal/pql/parser"
+)
+
+func TestSuggestCompletions(t *testing.T) {
+	tests := []struct {
+		name string
+
+		context      *AnalysisContext
+		sourceBefore string
+		sourceAfter  string
+
+		want []*Completion
+	}{
+		{
+			name: "Empty",
+			context: &AnalysisContext{
+				Tables: map[string]*AnalysisTable{
+					"foo": {
+						Columns: []*AnalysisColumn{
+							{Name: "id"},
+							{Name: "n"},
+						},
+					},
+					"bar": {
+						Columns: []*AnalysisColumn{
+							{Name: "id"},
+						},
+					},
+				},
+			},
+			sourceBefore: "",
+			sourceAfter:  "",
+			want: []*Completion{
+				{Identifier: "foo"},
+				{Identifier: "bar"},
+			},
+		},
+		{
+			name: "InitialSourceRef",
+			context: &AnalysisContext{
+				Tables: map[string]*AnalysisTable{
+					"foo": {
+						Columns: []*AnalysisColumn{
+							{Name: "id"},
+							{Name: "n"},
+						},
+					},
+					"bar": {
+						Columns: []*AnalysisColumn{
+							{Name: "id"},
+						},
+					},
+				},
+			},
+			sourceBefore: "f",
+			sourceAfter:  "",
+			want: []*Completion{
+				{Identifier: "foo"},
+			},
+		},
+		{
+			name: "SourceRefWithPipe",
+			context: &AnalysisContext{
+				Tables: map[string]*AnalysisTable{
+					"foo": {
+						Columns: []*AnalysisColumn{
+							{Name: "id"},
+							{Name: "n"},
+						},
+					},
+					"bar": {
+						Columns: []*AnalysisColumn{
+							{Name: "id"},
+						},
+					},
+				},
+			},
+			sourceBefore: "",
+			sourceAfter:  " | count",
+			want: []*Completion{
+				{Identifier: "foo"},
+				{Identifier: "bar"},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got := test.context.SuggestCompletions(test.sourceBefore+test.sourceAfter, parser.Span{
+				Start: len(test.sourceBefore),
+				End:   len(test.sourceBefore),
+			})
+			completionLess := func(a, b *Completion) bool {
+				return a.Identifier < b.Identifier
+			}
+			if diff := cmp.Diff(test.want, got, cmpopts.SortSlices(completionLess)); diff != "" {
+				t.Errorf("SuggestCompletions(...) (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
